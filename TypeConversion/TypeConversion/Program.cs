bfd05f884e0ef93cf3bb018ee57cf3f417653904
Tyler Beck <tbeck202@gmail.com>
2020-01-04 15:04:48 -0800

    commit practice files
    Studying fundamentals of variables and type conversion.

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            int i = 1000;
+            byte b = (byte)i; //implicitly casted with data loss
+            Console.WriteLine(b);
+
+            string number = "1234";
+            int ii = Convert.ToInt32(number);
+            Console.WriteLine(ii);
+
+            try
+            {
+                string numberTwo = "1234";
+                byte iii = Convert.ToByte(numberTwo);
+                Console.WriteLine(iii);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Number could not be coverted to byte");
+            }
+
+            try
+            {
+                string str = "true";
+                bool bb = Convert.ToBoolean(str);
+                Console.WriteLine(bb);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ variablesAndConstants/variablesAndConstants/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace variablesAndConstants
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'a';
+            var firstName = "Tyler";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
