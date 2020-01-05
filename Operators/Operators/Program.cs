fd62438f86fddbee663106f33a855a66b60224fa
Tyler Beck <tbeck202@gmail.com>
2020-01-05 13:57:03 -0800

    commit class practice

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -8,13 +8,21 @@ namespace Operators
         {
             var a = 10;
             var b = 3;
-            Console.WriteLine((float)a / (float)b);
+            var c = 3;
+            Console.WriteLine((float)a / (float)b); //3.333
+            Console.WriteLine(c);
 
             var aa = 1;
             var bb = 2;
             var cc = 3;
-            Console.WriteLine((aa + bb) * cc);
-            Console.WriteLine(aa > bb);
+            Console.WriteLine((aa + bb) * cc); //9
+            Console.WriteLine(aa > bb); // false
+            Console.WriteLine(aa == bb); // false
+            Console.WriteLine(!(aa != bb)); // false
+
+            Console.WriteLine(cc > bb && cc > aa); // true
+            Console.WriteLine(cc < bb || cc > aa); // true
+            Console.WriteLine(!(cc < bb || cc == aa)); // true
         }
     }
 }
---
+++ fundamentalsClasses/fundamentalsClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using fundamentalsClasses.Math;
+
+namespace fundamentalsClasses
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+            var mult = calculator.Multiply(5, 5);
+            Console.WriteLine(mult);
+        }
+    }
+}
