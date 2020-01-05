68d72d1832169ac6d171e2440329b9d89cc06ebd
Tyler Beck <tbeck202@gmail.com>
2020-01-05 09:44:38 -0800

    commit operators practice

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine((float)a / (float)b);
+
+            var aa = 1;
+            var bb = 2;
+            var cc = 3;
+            Console.WriteLine((aa + bb) * cc);
+            Console.WriteLine(aa > bb);
+        }
+    }
+}
