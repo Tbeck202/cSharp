6bca3e65e2eeac1b7c3530947b22a198bf077311
Tyler Beck <tbeck202@gmail.com>
2020-01-05 17:33:42 -0800

    commit enum practice

---
+++ fundamentals/Enums/Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // declare an enum when you have more than one related constant
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method); //casted to int
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine((int)shippingMethod);
+        }
+    }
+}
