663023235ee7ec5e3d616ad75dab945f24b2baa6
Tyler Beck <tbeck202@gmail.com>
2020-01-07 20:32:19 -0800

    commit exercise 1
    Simple program with an if/else statement

---
+++ fundamentals/exercises/conditionalEx1/conditionalEx1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace conditionalEx1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input < 0 || input > 10)
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("Valid!");
+            }
+        }
+    }
+}
