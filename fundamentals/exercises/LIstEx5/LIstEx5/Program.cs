fe8a972e5d8e0bc1c859eb701b08355c5130bba0
Tyler Beck <tbeck202@gmail.com>
2020-01-13 18:54:55 -0800

    Commit list ex 5

---
+++ fundamentals/exercises/LIstEx5/LIstEx5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LIstEx5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var running = true;
+            while (running)
+            {
+                Console.WriteLine("Please enter some numbers, seperated by a comma!");
+                var input = Console.ReadLine();
+                char[] seperators = { ',', ' ' };
+
+                string[] nums = input.Split(seperators);
+                var convertedNums = new List<int>();
+
+                if (String.IsNullOrWhiteSpace(input) || nums.Length < 5)
+                {
+                    Console.WriteLine("Invalid list! Please try again.");
+                    continue;
+                }
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    var num = Convert.ToInt32(nums[i]);
+                    convertedNums.Add(num);
+                }
+                convertedNums.Sort();
+                Console.WriteLine("The three smallest numbers are: {0}, {1}, {2}", convertedNums[0], convertedNums[1], convertedNums[2]);
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+                running = false;
+            } 
+        }
+    }
+}
