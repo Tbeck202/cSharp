ebcacef306f9112228aeb675caa6e770961c9e41
Tyler Beck <tbeck202@gmail.com>
2020-01-14 20:28:16 -0800

    commit string practice

---
+++ fundamentals/StringsInDepth2/StringsInDepth2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace StringsInDepth2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really really really really really long text";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
+        }
+    }
+}
