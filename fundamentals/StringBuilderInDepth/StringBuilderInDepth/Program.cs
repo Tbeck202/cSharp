1e4e56de36ef69bed0531a0a79fc6116247f5b0d
Tyler Beck <tbeck202@gmail.com>
2020-01-14 20:58:05 -0800

    commit StringBuilder practice

---
+++ fundamentals/StringBuilderInDepth/StringBuilderInDepth/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderInDepth
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello world!");
+
+            builder
+                .AppendLine()
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+            Console.WriteLine(builder);
+
+            builder.Replace('-', '+');
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: " + builder[0]);
+        }
+    }
+}
