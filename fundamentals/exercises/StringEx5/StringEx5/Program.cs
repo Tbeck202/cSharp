0c7cb00b874ee38033ca99ee2da70aa0d56c57c2
Tyler Beck <tbeck202@gmail.com>
2020-01-18 13:00:04 -0800

    Commit StrinEx5

---
+++ fundamentals/exercises/StringEx5/StringEx5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringEx5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write a word!");
+            var input = Console.ReadLine().ToLower();
+            var characters = new List<char>();
+            var count = 0;
+
+            foreach(var letter in input)
+            {
+                characters.Add(letter);
+            }
+
+            foreach(var item in characters)
+            {
+                if (item == 'a' ||
+                    item == 'e' ||
+                    item == 'i' ||
+                    item == 'o' ||
+                    item == 'u')
+                {
+                    count++;
+                } 
+            }
+            Console.WriteLine("There were {0} vowels in your word!", count);
+        }
+    }
+}
